4448a8f2a31e3501f6c2fd1a4f0558ae6c5dd9b2
A-lineed <aline.edvania.franca@outlook.com>
2025-05-09 17:17:44 -0300

    Configurando ESlint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// eslint.config.js
+
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js },
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'no-trailing-spaces': ['error']
+    },
+    extends: ['js/recommended']
+  },
+  pluginReact.configs.flat.recommended
+])
