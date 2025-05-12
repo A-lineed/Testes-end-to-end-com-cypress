ad3f0bc04fe3288c33f625fa89f018fac3afe234
A-lineed <aline.edvania.franca@outlook.com>
2025-05-12 17:49:54 -0300

    Utilizando Mailosaur para teste de confirmação de código via e-mail

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://notes-serverless-app.com/',
+    //defaultCommandTimeout: 10000, -> Altera o timeout padrão do cypress para 10 segundos ao invés de 4 que é o padrão
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 // eslint.config.js
 
-import js from '@eslint/js'
+/* import js from '@eslint/js'
 import globals from 'globals'
 import pluginReact from 'eslint-plugin-react'
-import { defineConfig } from 'eslint/config'
+import { defineConfig } from 'eslint/config' */
 
 export default defineConfig([
   {
