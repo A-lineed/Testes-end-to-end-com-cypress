65c16dbcf8ebb55df1932b8fd373d24e7ff64350
A-lineed <aline.edvania.franca@outlook.com>
2025-05-12 18:30:43 -0300

    Guardando sessão do login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,11 +2,8 @@
 
 describe('Login', () => {
     it('successfully logs in', () => {
-        cy.intercept('GET', '**/notes').as('getNotes')
+        cy.guiLogin()
 
-        cy.login()
-        cy.wait('@getNotes')
-        
         cy.contains('a', 'Create a new note').should('be.visible')
     })
 })
