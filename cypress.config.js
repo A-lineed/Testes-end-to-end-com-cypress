770d453293cadf92389b163e4044d6d6424f6035
A-lineed <aline.edvania.franca@outlook.com>
2025-05-13 17:13:06 -0300

    Testando um formulário de cartão de crédito renderizado dentro de um iFrame

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
-    baseUrl: 'https://notes-serverless-app.com/',
+    baseUrl: 'https://notes-serverless-app.com',
     //defaultCommandTimeout: 10000, -> Altera o timeout padrão do cypress para 10 segundos ao invés de 4 que é o padrão
     setupNodeEvents(on, config) {
       // implement node event listeners here
