87747eacb91e893fae8dd779c2f53ea1fa20ab35
A-lineed <aline.edvania.franca@outlook.com>
2025-05-13 16:25:24 -0300

    Criando comandos customizados para o teste do CRUD

--- cypress/e2e/crud.cy.js
+++ cypress/e2e/crud.cy.js
@@ -3,58 +3,22 @@
 import { faker } from '@faker-js/faker/locale/en'
 
 describe('CRUD', () => {
-    it('CRUDs a note', () => {
-        const noteDescription = faker.lorem.words(4)
-        let attachFile = false
+  it('CRUDs a note', () => {
+    const noteDescription = faker.lorem.words(4)
 
-        cy.intercept('GET', '**/notes').as('getNotes')
-        cy.intercept('GET', '**/notes/**').as('getNote')
-        cy.sessionLogin()
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.sessionLogin()
 
-        cy.visit('/notes/new')
-        cy.get('#content').type(noteDescription)
+    cy.createNote(noteDescription)
+    cy.wait('@getNotes')
 
-        if (attachFile) {
-            cy.get('#file').selectFile('cypress/fixtures/example.json')
-        }
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
 
-        cy.contains('button', 'Create').click()
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait('@getNotes')
 
-        cy.wait('@getNotes')
-        cy.contains('.list-group-item', noteDescription)
-            .should('be.visible')
-            .click()
-        cy.wait('@getNote')
-
-        const updatedNoteDescription = faker.lorem.words(4)
-
-        cy.get('#content')
-            .as('contentField')
-            .clear()
-        cy.get('@contentField')
-            .type(updatedNoteDescription)
-
-        attachFile = true
-
-        if (attachFile) {
-            cy.get('#file').selectFile('cypress/fixtures/example.json')
-        }
-
-        cy.contains('button', 'Save').click()
-        cy.wait('@getNotes')
-
-        cy.contains('.list-group-item', noteDescription).should('not.exist')
-        cy.contains('.list-group-item', updatedNoteDescription)
-            .should('be.visible')
-            .click()
-        cy.wait('@getNote')
-        cy.contains('button', 'Delete').click()
-        cy.wait('@getNotes')
-
-        cy.get('.list-group-item')
-            .its('length')
-            .should('be.at.least', 1)
-        cy.contains('.list-group-item', updatedNoteDescription)
-            .should('not.exist')
-    })
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNotes')
+  })
 })
