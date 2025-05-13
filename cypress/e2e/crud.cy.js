04dc7c426add8a8975bb24ad14367c40d0200192
A-lineed <aline.edvania.franca@outlook.com>
2025-05-13 16:23:41 -0300

    Inserindo arquivo

--- cypress/e2e/crud.cy.js
+++ cypress/e2e/crud.cy.js
@@ -3,45 +3,58 @@
 import { faker } from '@faker-js/faker/locale/en'
 
 describe('CRUD', () => {
-  it('CRUDs a note', () => {
-    const noteDescription = faker.lorem.words(4)
+    it('CRUDs a note', () => {
+        const noteDescription = faker.lorem.words(4)
+        let attachFile = false
 
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.intercept('GET', '**/notes/**').as('getNote')
-    cy.sessionLogin()
+        cy.intercept('GET', '**/notes').as('getNotes')
+        cy.intercept('GET', '**/notes/**').as('getNote')
+        cy.sessionLogin()
 
-    cy.visit('/notes/new')
-    cy.get('#content').type(noteDescription)
-    cy.contains('button', 'Create').click()
+        cy.visit('/notes/new')
+        cy.get('#content').type(noteDescription)
 
-    cy.wait('@getNotes')
-    cy.contains('.list-group-item', noteDescription)
-      .should('be.visible')
-      .click()
-    cy.wait('@getNote')
+        if (attachFile) {
+            cy.get('#file').selectFile('cypress/fixtures/example.json')
+        }
 
-    const updatedNoteDescription = faker.lorem.words(4)
+        cy.contains('button', 'Create').click()
 
-    cy.get('#content')
-      .as('contentField')
-      .clear()
-    cy.get('@contentField')
-      .type(updatedNoteDescription)
-    cy.contains('button', 'Save').click()
-    cy.wait('@getNotes')
+        cy.wait('@getNotes')
+        cy.contains('.list-group-item', noteDescription)
+            .should('be.visible')
+            .click()
+        cy.wait('@getNote')
 
-    cy.contains('.list-group-item', noteDescription).should('not.exist')
-    cy.contains('.list-group-item', updatedNoteDescription)
-      .should('be.visible')
-      .click()
-    cy.wait('@getNote')
-    cy.contains('button', 'Delete').click()
-    cy.wait('@getNotes')
+        const updatedNoteDescription = faker.lorem.words(4)
 
-    cy.get('.list-group-item')
-      .its('length')
-      .should('be.at.least', 1)
-    cy.contains('.list-group-item', updatedNoteDescription)
-      .should('not.exist')
-  })
+        cy.get('#content')
+            .as('contentField')
+            .clear()
+        cy.get('@contentField')
+            .type(updatedNoteDescription)
+
+        attachFile = true
+
+        if (attachFile) {
+            cy.get('#file').selectFile('cypress/fixtures/example.json')
+        }
+
+        cy.contains('button', 'Save').click()
+        cy.wait('@getNotes')
+
+        cy.contains('.list-group-item', noteDescription).should('not.exist')
+        cy.contains('.list-group-item', updatedNoteDescription)
+            .should('be.visible')
+            .click()
+        cy.wait('@getNote')
+        cy.contains('button', 'Delete').click()
+        cy.wait('@getNotes')
+
+        cy.get('.list-group-item')
+            .its('length')
+            .should('be.at.least', 1)
+        cy.contains('.list-group-item', updatedNoteDescription)
+            .should('not.exist')
+    })
 })
